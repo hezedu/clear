9d4239673028aa9085e1e3b6831153a8562b757f
hezedu@hotmail.com <hezedu@hotmail.com>
2016-09-08 15:17:17 +0800

    index href && css

--- src/cmpt/layout.jsx
+++ src/cmpt/layout.jsx
@@ -69,9 +69,12 @@ export class Home extends Component {
       <div className={style.homeTitle}>
         <big>Clear</big><small>前端架构</small>
         <br/>
-        webpack + sass + possCss
-         + <a href='https://github.com/reactjs/react-router/tree/master/docs'>react-router</a>
-         + react + redux
+        <a href='http://webpack.github.io/docs/' target='_blank'>webpack</a>
+         + <a href='http://sass-lang.com/documentation/file.SASS_REFERENCE.html#css_extensions' target='_blank'>sass</a>
+         + <a href='http://simplaio.github.io/rucksack/docs/#autoprefixing' target='_blank'>possCss</a>
+         + <a href='https://github.com/reactjs/react-router' target='_blank'>react-router</a>
+         + <a href='https://facebook.github.io/react/docs/getting-started.html' target='_blank'>react</a>
+         + <a href='http://redux.js.org/index.html' target='_blank'>redux</a>
         </div>
     );
   }

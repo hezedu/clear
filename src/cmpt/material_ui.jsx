1ccd5210af13783bd157dd360957a098aea5fb41
hezedu <hezedu@hotmail.com>
2016-09-12 22:57:26 +0800

    改动态路由

--- src/cmpt/material_ui.jsx
+++ src/cmpt/material_ui.jsx
@@ -1,8 +1,10 @@
 import { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import AppBar from 'material-ui/AppBar';
-import IconButton from 'material-ui/IconButton';
-
+import {Tabs, Tab} from 'material-ui/Tabs';
+import FlatButton from 'material-ui/FlatButton';
+import {Link} from 'react-router';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
@@ -11,15 +13,29 @@ if(!injectTapEventPlugin.called){
   injectTapEventPlugin();
 }
 
-export class Theme extends Component{
+const muiTheme = getMuiTheme({
+  fontFamily:["Microsoft YaHei", "Hiragino Sans GB", "Courier new"]
+  // palette: {
+  //   textColor: cyan500,
+  // },
+  // appBar: {
+  //   height: 50,
+  // },
+});
+
+const TabsStyle = {
+  width:'auto'
+}
+export  default class Theme extends Component{
   render(){
     return (
-      <MuiThemeProvider>
-      <AppBar title='前端架构' iconElementRight={<IconButton
-              iconClassName="muidocs-icon-custom-github"
-              href="https://github.com/callemall/material-ui"
-            />}>
-      hehe
+      <MuiThemeProvider muiTheme={muiTheme}>
+      <AppBar title={<Tabs style={TabsStyle}>
+              <Tab label={<Link to = "/">Item One</Link>} />
+              <Tab label="Item Two" />
+              <Tab label="onActive" data-route="/home"/>
+              </Tabs>}  iconElementRight={<FlatButton label="Save" />}>
+
       </AppBar>
       </MuiThemeProvider>
     );
--- src/router.config.js
+++ src/router.config.js
@@ -1,7 +1,6 @@
 import Home from './cmpt/home.jsx';
 import List from './cmpt/list.jsx';
 import Detail from './cmpt/detail.jsx';
-import * as Mui from './cmpt/material_ui.jsx';
 
 const dynamicRouting = {
   path: 'dynamic_routing',
@@ -13,6 +12,17 @@ const dynamicRouting = {
   }
 };
 
+const mUidynamicRouting = {
+  path: 'material_ui',
+  title: 'material-ui',
+  getComponents: function(nextState, callback) {
+    require(['./cmpt/material_ui'], function (cmpt) {
+      callback(null, cmpt.default);
+    });
+  }
+};
+
+
 export default {
   path: '/',
   indexRoute: { component: Home },
@@ -20,9 +30,6 @@ export default {
     {path: 'list', component: List, title: '列表'},
     {path: 'detail/:id', component: Detail, title: '详情'},
     dynamicRouting ,
-    {path: 'material_ui', component: Mui.Theme, title: 'material-ui',
-      childRoutes:[
-        //{path : 'app_bar', component: Mui.AppBar, title: 'AppBar'}
-      ]}
+    mUidynamicRouting,
   ]
 };

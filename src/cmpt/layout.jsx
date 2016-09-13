8f91b1be800f250e51b4e832e8f16760ba358870
hezedu@hotmail.com <hezedu@hotmail.com>
2016-09-13 14:58:56 +0800

    layout

--- src/cmpt/layout.jsx
+++ src/cmpt/layout.jsx
@@ -5,9 +5,17 @@ export class Top extends Component {
   render() {
     return (
       <div>
-        <div className="top"><Link to='/'>home</Link></div>
+        <div className="top"><Link to='/' onlyActiveOnIndex={true} activeClassName='active'>home</Link></div>
         <div className="bottom">{this.props.children}</div>
       </div>
+    );
+  }
+}
+
+export class NotFound extends Component {
+  render() {
+    return (
+      <h1>404 NotFound</h1>
     );
   }
 }
--- src/router.config.js
+++ src/router.config.js
@@ -1,8 +1,11 @@
 import Home from './cmpt/home';
-import layout from './cmpt/layout';
+import * as  layout from './cmpt/layout';
 
 export default {
   path: '/',
   component: layout.Top,
-  indexRoute: { component: Home }
+  indexRoute: { component: Home },
+  childRoutes:[
+    {path:'*', component: layout.NotFound}
+  ]
 };
